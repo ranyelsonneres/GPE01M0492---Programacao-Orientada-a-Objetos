25b637622e2505ce58974be736acae069b10fa23
Ranyelson Neres <ranyelson.carvalho@p.ucb.br>
2024-08-31 11:06:57 -0300

    Create Calculadora.java

---
+++ Aula_31_08_2024/calculadora/Calculadora.java
@@ -0,0 +1,18 @@
+package calculadora;
+
+public class Calculadora {
+	
+	//atributos
+	double num1;
+	double num2;
+	
+	//métodos
+	public double soma() {
+		return num1 + num2;
+	}
+	
+	public double subtrair() {
+		return num1 - num2;
+	}
+
+}
