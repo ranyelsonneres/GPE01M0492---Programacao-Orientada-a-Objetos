0d90a907f0cb6c10261a6855566a28b5e7cf1123
Ranyelson Neres <ranyelson.carvalho@p.ucb.br>
2024-09-21 11:20:08 -0300

    Create Principal_m1.java

---
+++ Aula_21_09_2024/modificadores/m1/Principal_m1.java
@@ -0,0 +1,14 @@
+package m1;
+
+public class Principal_m1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Carro carro2 = new Carro("Fusca", 50000, 1000);
+		carro2.quilometragem = 10;
+		
+		carro2.exibirkm();
+
+	}
+
+}
