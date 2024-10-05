1ce2f396ab6a15b41e1d601c60685439676dccfa
Ranyelson Neres <ranyelson.carvalho@p.ucb.br>
2024-10-05 11:04:13 -0300

    Create Aluno.java

---
+++ Aula_05_10_2024/heranca/Aluno.java
@@ -0,0 +1,27 @@
+package heranca;
+
+public class Aluno extends Pessoa{
+	
+	private String curso;
+
+	public Aluno(String nome, int idade, String curso) {
+		super(nome, idade);
+		this.curso = curso;
+	}
+
+	public String getCurso() {
+		return curso;
+	}
+
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+	
+	public void apresentar() {
+		super.apresentar();
+		System.out.println("Curso: " + curso);
+	}
+	
+	
+
+}
